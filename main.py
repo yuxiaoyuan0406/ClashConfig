d93f5ff379a93ee0aa40838c281a60ce2adba2db
yuxiuaoyuan0406 <973971990@qq.com>
2023-12-21 16:21:06 +0800

    finish major function

---
+++ main.py
@@ -0,0 +1,49 @@
+import yaml
+import argparse
+
+  
+
+def main():
+    parser = argparse.ArgumentParser(description="这是一个示例脚本，用于解释如何使用argparse。")
+
+    parser.add_argument('--input', help="Directory to input file.", default='config.yaml')
+    parser.add_argument('--output', help="Directory to output file.", default="output.yaml")
+    # parser.add_argument('--verbose', help="增加输出的详细程度", action='store_true')
+
+    args = parser.parse_args()
+
+    input_file = args.input
+    output_file = args.output
+
+    # read from YAML file
+    with open(input_file, 'r') as file:
+        data = yaml.safe_load(file)
+
+    # default proxy group
+    default_proxy_group = {
+        "name": "default",
+        "type": "url-test",
+        "url": "http://www.gstatic.com/generate_204",
+        "interval": 300,
+        "proxies": []
+    }
+
+    # edit data
+    ## create a auto switch group for United States proxies
+    proxy_names = [proxy['name'] for proxy in data['proxies']]
+    US_proxy_names = [name for name in proxy_names if 'UnitedStates' in name]
+    
+    US_proxy_group = default_proxy_group
+    US_proxy_group['name'] = 'US-Auto'
+    US_proxy_group['proxies'] = US_proxy_names
+    data['proxy-groups'].append(US_proxy_group)
+    ## change allow lan
+    data['allow-lan'] = True
+    ## chane log level
+    data['log-level'] = 'debug'
+    
+    # 将修改后的数据写回文件
+    with open(output_file, 'w') as file:
+        yaml.dump(data, file)
+
+main()
