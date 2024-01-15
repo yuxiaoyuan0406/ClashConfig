46fe0cd7ca45e9c1c5a2e392f4a15a790ba8de58
yuxiaoyuan0406 <973971990@qq.com>
2024-01-15 17:14:19 +0800

    structure change

--- main.py
+++ main.py
@@ -24,21 +24,7 @@ def get_config():
             return config
     return {}
 
-
-def main():
-    parser = argparse.ArgumentParser(description="Set clash config filr.")
-
-    parser.add_argument('--input', help="Directory to input file.", default='config.yaml')
-    parser.add_argument('--output', help="Directory to output file.", default="output.yaml")
-    parser.add_argument('--update', help='Update from env.CLASH_URL', action='store_true')
-    # parser.add_argument('--verbose', help="增加输出的详细程度", action='store_true')
-
-    args = parser.parse_args()
-
-    input_file = args.input
-    output_file = args.output
-    do_update = args.update
-    
+def edit_config(input_file, output_file, do_update=True):
     if do_update:
         data = get_config()
         if data:
@@ -107,4 +93,23 @@ def main():
         yaml.dump(data, file)
         file.close()
 
-main()
+
+def main():
+    parser = argparse.ArgumentParser(description="Set clash config filr.")
+
+    parser.add_argument('--input', help="Directory to input file.", default='config.yaml')
+    parser.add_argument('--output', help="Directory to output file.", default="output.yaml")
+    parser.add_argument('--update', help='Update from env.CLASH_URL', action='store_true')
+    # parser.add_argument('--verbose', help="增加输出的详细程度", action='store_true')
+
+    args = parser.parse_args()
+
+    input_file = args.input
+    output_file = args.output
+    do_update = args.update
+
+    edit_config(args.input, args.output, args.update)
+
+
+if __name__ == '__main__':
+    main()
